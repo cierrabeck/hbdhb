fc6eec6b4950134da4f3d3b94d7aa3b99852a0f8
cierrabeck <cnb44@cornell.edu>
2020-11-19 10:22:57 -0500

    add font, base image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import card from './assets/card.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={card} className="App-logo" alt="logo" />
+        <p className="rules">
+          The rules
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
